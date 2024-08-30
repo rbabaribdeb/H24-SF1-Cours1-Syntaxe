6f3410ddbaad316997078d5323310dc13cdb7fbc
RBabari <raouf.babari@bdeb.qc.ca>
2024-08-30 11:45:24 -0400

    ajout main6 Exercice aire

---
+++ src/Main6.java
@@ -0,0 +1,19 @@
+
+// 6 - Reprendre l'exemple de calcul de rayon
+
+public class Main6 {
+    public static void main(String[] args) {
+// DEBUT
+     // Déclarations des variables & constantes
+
+    double rayon = 10; // variable
+    final double PI = 3.14; // constante
+    double aire = 0;
+
+    // Algorithme ---------------------------------------
+    aire = PI * rayon * rayon;
+    System.out.println("L'aire est " + aire); // concatenation
+
+//FIN
+    }
+}
