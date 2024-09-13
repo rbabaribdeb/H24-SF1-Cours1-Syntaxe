ae7c24c575a1ec2b97a710a8bc3740840424b826
rbabari <r.babari@gmail.com>
2024-09-13 11:25:52 -0400

    test condition 1 et 2

---
+++ src/MainCondition1.java
@@ -0,0 +1,32 @@
+/*
+Afficher à quelle catégorie (enfant/adolescent/adulte)
+de personnes appartient un individu en fonctionde son âge.
+L’âge est saisi au clavier.
+Décrire les étapes de votre algorithme creately.com
+ */
+public class MainCondition1 {
+    public static void main(String[] args) {
+    //DEBUT
+    int age = -3;
+    boolean estEnfant = age < 12 && age > 0;
+    boolean estAdo = age >= 12 && age <= 18;
+    boolean estAdulte = age >= 18 && age <= 125; // ***
+    //-------------------------------
+        if (estEnfant)
+        {
+            System.out.println("Bonjour Enfant");
+        }
+        if (estAdo)
+        {
+            System.out.println("Bonjour Ado");
+        }
+        if (estAdulte)
+        {
+            System.out.println("Bonjour adulte");
+        }
+
+
+//FIN
+    }
+
+}
---
+++ src/MainCondition2.java
@@ -0,0 +1,28 @@
+/*
+Afficher à quelle catégorie (enfant/adolescent/adulte)
+de personnes appartient un individu en fonctionde son âge.
+L’âge est saisi au clavier.
+Décrire les étapes de votre algorithme creately.com
+ */
+public class MainCondition2 {
+    public static void main(String[] args) {
+    //DEBUT
+    int age = -3; // Code avec erreur
+
+    //-------------------------------
+        if (age > 0 && age < 12)
+        {
+            System.out.println("Bonjour Enfant");
+        }
+        else if (age < 18)
+        {
+            System.out.println("Bonjour Ado");
+        }
+        else
+        {
+            System.out.println("Bonjour adulte");
+        }
+//FIN
+    }
+
+}
