11ce2b4a029234ee19689b38e543d4c4277e6a63
RBabari <raouf.babari@bdeb.qc.ca>
2024-09-20 10:07:03 -0400

    Organisation

---
+++ src/rec.java
@@ -0,0 +1,25 @@
+public class rec {
+}
+
+/*
+Madame/Monsieur,
+
+Je suis heureux de recommander chaleureusement Mme Noémie Nametchougli, actuellement étudiante en première session en Science informatique et mathématiques au Collège Bois de Boulogne.
+ Depuis le début de la session, j'ai eu le plaisir d'encadrer Mme Nametchougli dans le cours 420-SF1-BB, axé sur la programmation orientée Objets.
+
+Mme Nametchougli se distingue par son sérieux, son assiduité et sa forte motivation pour le développement informatique. Tout au long de cet encadrement, j'ai pu constater son implication à aider ses collègues,
+ainsi que sa rigueur et sa persévérance, des qualités qu'elle a démontrées de manière constante.
+
+En raison de ses compétences exceptionnelles et de son dévouement, je soutiens avec enthousiasme sa candidature à la bourse d'étude FORTES.
+
+Je suis convaincu qu'elle continuera à exceller et à apporter une contribution significative dans son domaine.
+
+Je reste à votre disposition pour toute information complémentaire.
+Veuillez agréer, Madame/Monsieur, l'expression de mes salutations distinguées.
+
+Raouf Babari
+Enseignant Ph.D
+raouf.babari@bdeb.qc.ca.
+Collège de Bois de Boulogne
+Montréal, le 17 septembre 2024
+* */
