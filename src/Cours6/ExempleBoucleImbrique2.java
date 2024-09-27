4fcdb24d6f95758082375e35bd2f59469f72db06
RBabari <raouf.babari@bdeb.qc.ca>
2024-09-27 11:36:35 -0400

    tableau de multiplication

--- src/Cours6/ExempleBoucleImbrique2.java
+++ src/Cours6/ExempleBoucleImbrique2.java
@@ -1,12 +1,12 @@
 package Cours6;
 
-public class ExempleBoucleImbrique1
+public class ExempleBoucleImbrique2
 {
     public static void main(String[] args)
     {
-        for (int i = 0 ; i< 10 ; i ++) {
-            for (int j = 0; j < 30; j++) {
-                System.out.print("*");
+        for (int i = 0 ; i<= 10 ; i ++) {
+            for (int j = 0; j <= 10; j++) {
+                System.out.print(i*j + " ");
             }//fin boucle j
 
         System.out.println();
