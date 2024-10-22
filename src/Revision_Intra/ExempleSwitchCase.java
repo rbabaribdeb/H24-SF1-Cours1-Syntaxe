9465b0daa61e41fb5c6ff9313f5170695556c8a6
RBabari <raouf.babari@bdeb.qc.ca>
2024-10-22 16:32:41 -0400

    Exercice switch

---
+++ src/Revision_Intra/ExempleSwitchCase.java
@@ -0,0 +1,37 @@
+package Revision_Intra;
+
+import java.util.Scanner;
+
+public class ExempleSwitchCase {
+    public static void main(String[] args) {
+       // Utiliser break pour éviter l'exécution des case suivants.
+       // Ne pas utiliser break si tu souhaites exécuter plusieurs blocs de code consécutivement.
+
+        // Qu'affiche ce code ?
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Entrez un nombre entre 1 et 4 : ");
+        int number = scanner.nextInt();
+
+        switch (number) {
+            case 1:
+                System.out.println("Vous avez choisi le nombre un.");
+                break; // Sortir après avoir exécuté ce case
+            case 2:
+                System.out.println("Vous avez choisi le nombre deux.");
+                // Pas de break ici, donc le code continuera à s'exécuter
+            case 3:
+                System.out.println("Vous avez choisi le nombre trois.");
+                break; // Sortir après avoir exécuté ce case
+            case 4:
+                System.out.println("Vous avez choisi le nombre quatre.");
+                break; // Sortir après avoir exécuté ce case
+            default:
+                System.out.println("Nombre invalide, veuillez entrer un nombre entre 1 et 4.");
+                break;
+        }
+
+        scanner.close();
+
+    }
+}
