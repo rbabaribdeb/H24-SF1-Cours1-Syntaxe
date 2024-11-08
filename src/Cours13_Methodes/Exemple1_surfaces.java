ee14591f8dce90413f1e016c6a67724422b1e1cd
RBabari <raouf.babari@bdeb.qc.ca>
2024-11-08 10:53:09 -0500

    Exercice methodes

---
+++ src/Cours13_Methodes/Exemple1_surfaces.java
@@ -0,0 +1,38 @@
+package Cours13_Methodes;
+
+public class Exemple1_surfaces {
+    public static void main(String[] args) {
+
+        double rayon1=1, rayon2=2,rayon3=3,rayon4=4,rayon5=5;
+        double circonference;
+        double surface;
+        //-------------
+        circonference=2*rayon1*3.14 ;
+        surface= rayon1*rayon1*3.14 ;
+        System.out.println("La circonference pour R= "+rayon1+" est: "+ circonference) ;
+        System.out.println("La surface pour R= "+surface) ;
+        //---------------
+        circonference=2*rayon2*3.14 ;
+        surface= rayon2*rayon2*3.14 ;
+        System.out.println("La circonference pour R= "+rayon2+" est: "+ circonference) ;
+        System.out.println("La surface pour R= "+surface) ;
+        //----------------
+        circonference=2*rayon3*3.14 ;
+        surface= rayon3*rayon3*3.14 ;
+        System.out.println("La circonference pour R= "+rayon3+" est: "+ circonference) ;
+        System.out.println("La surface pour R= "+surface) ;
+        //-----------------
+        circonference=2*rayon4*3.14 ;
+        surface= rayon4*rayon4*3.14 ;
+        System.out.println("La circonference pour R= "+rayon4+" est: "+ circonference) ;
+        System.out.println("La surface pour R= "+surface) ;
+        circonference=2*rayon5*3.14 ;
+        surface= rayon5*rayon5*3.14 ;
+        System.out.println("La circonference pour R= "+rayon5+" est: "+ circonference) ;
+        System.out.println("La surface pour R= "+surface) ;
+
+
+    }
+
+
+}
